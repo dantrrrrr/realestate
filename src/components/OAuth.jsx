7c4e7176eb22532aa61b9c0ac701260524dbbe62
dantr <huynhngdantr@gmail.com>
2023-11-21 13:14:53 +0700

    login with google - DONE sync account with database- DONE

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,7 +1,11 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
-
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { signInSuccess, useUserSelector } from "../redux/user/userSlice";
 export default function OAuth() {
+  const dispatch = useDispatch();
+  const { loading, error } = useUserSelector();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -9,7 +13,16 @@ export default function OAuth() {
       const auth = getAuth(app);
 
       const result = await signInWithPopup(auth, provider);
-      ///TODO make sync with database account
+
+      const data = {
+        name: result.user.displayName,
+        email: result.user.email,
+        avatar: result.user.photoURL,
+      };
+
+      const response = await axios.post("/api/auth/google", data);
+
+      dispatch(signInSuccess(response.data));
     } catch (error) {
       console.log("Could not sign in with Google", error);
     }
