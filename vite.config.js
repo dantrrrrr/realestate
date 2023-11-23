e8522ad634569a996ba326eb576cb1cbdbfd5998
dantr <huynhngdantr@gmail.com>
2023-11-23 22:14:28 +0700

    test deploy 06

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { vitePluginReactIcons } from "vite-plugin-react-icons";
+import reactIcons from "vite-plugin-react-icons";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 const API_URL = "https://api-realestate-blue.vercel.app";
@@ -11,7 +11,7 @@ export default defineConfig({
       "/api": { target: API_URL, secure: false },
     },
   },
-  plugins: [reactRefresh(), vitePluginReactIcons()],
+  plugins: [reactRefresh(),reactIcons],
   build: {
     rollupOptions: {
       external: ["react-icons"],
